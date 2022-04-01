fe3de78b592e369ce6181a5974cd5b1b7fdfa198
Juan Alvarado <jalvarado91@gmail.com>
2022-03-31 20:59:42 -0400

    Pass 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,12 +50,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           type="image/png"
           sizes="16x16"
         />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
