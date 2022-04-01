d97821fc758bc46d4c862818cac706a033fecfde
Juan Alvarado <jalvarado91@gmail.com>
2022-03-31 23:12:44 -0400

    Add stream urls query

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,8 @@
 import { Context, createContext } from "@/server/context";
+import { createSoundCloudApiClient } from "@/server/crosscutting/soundCloudApiClient";
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
+import { ObjectId } from "mongodb";
 import { z } from "zod";
 import { getSoundCloudTracks } from "../internal/sync-episodes";
 
@@ -38,6 +40,29 @@ const appRouter = trpc
         .toArray();
 
       return allTracks;
+    },
+  })
+  .query("episode.getStreamUrl", {
+    input: z.object({
+      episodeId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const { episodeId } = input;
+
+      const trackCollection = ctx.db.collection<ITrack>("tracksOld");
+      const episode = await trackCollection.findOne({
+        _id: new ObjectId(episodeId),
+      });
+
+      if (!episode) {
+        return null;
+      }
+
+      const scTrackId = `${episode.key}`;
+      const scClient = await createSoundCloudApiClient();
+      const streamUrls = await scClient.getStreamUrls(scTrackId);
+
+      return streamUrls;
     },
   });
 
