616bd3f003252ad29ac41498f0c3b21f94d46bec
Juan Alvarado <jalvarado91@gmail.com>
2022-04-01 20:34:17 -0400

    Nit

--- src/client/components/Icons.tsx
+++ src/client/components/Icons.tsx
@@ -161,9 +161,7 @@ export function IconDotsHorizontal(props: any) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="h-6 w-6"
-      // fill="none"
       viewBox="0 0 24 24"
-      // stroke="currentColor"
       {...props}
       strokeWidth={2}
     >
